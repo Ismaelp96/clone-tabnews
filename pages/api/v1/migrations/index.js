5dd9e28352061856f9c4a10b5ad2c469cfc81d11
ismaelp96 <ismael6991@live.com>
2024-05-09 09:55:20 -0300

    Expandindo testes e refatorando implementação

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,28 +2,32 @@ import { join } from "node:path";
 import migrationRunner from "node-pg-migrate";
 
 export default async function migrations(request, response) {
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: false,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    response.status(200).json(migrations);
+    const pedingMigrations = await migrationRunner(defaultMigrationOptions);
+    response.status(200).json(pedingMigrations);
   }
 
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migrateMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    response.status(200).json(migrations);
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
+    } else {
+      return response.status(200).json(migrateMigrations);
+    }
   }
+
   return response.status(405).end(); // Method not Allowed
 }
